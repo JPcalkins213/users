48905d4aadb9e3b9e61e005cc11d3005d5256439
Jessica Calkins <jessicacalkins795213@gmail.com>
2021-06-18 10:33:20 -0500

    this is for a later assignment

---
+++ user.py
@@ -0,0 +1,26 @@
+class User:
+    bank_name = 'First national Dojo'
+    def __init__(self, name, email_address):
+        self.name = name
+        self.email = email_address
+        self.account_balance = 0
+    def make_deposit(self,amount):
+        self.account_balance += amount
+        return self
+    def make_withdrawl(self, amount):
+        self.account_balance -= amount
+        return self
+    def display_user_balance(self):
+        print(self.name) 
+        print(self.account_balance)
+        return self
+    def transfer_money(self, other_user, ammount):
+        self.account_balance -= ammount
+        ammount += other_user
+        other_user = self.account_balance
+        return self
+
+
+guido = User('Guido van Rossum', 'guido@python.com')
+monty = User('Monty Python', 'monty@python.com')
+guido.make_deposit(100).make_deposit(200).make_deposit(50).make_withdrawl(25).display_user_balance()
